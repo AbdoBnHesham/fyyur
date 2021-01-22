1c4eecb93bbbf5e502c67ca19cc2c3afa2576702
AbdullahHB <abdullahhesham@protonmail.com>
2021-01-22 07:35:03 +0200

    Refactor flask configs and mark some TODOs as finished
    - Get secret key from .env file.
    - Comment debug in config.py as it's not working "not sure why" but it works from .env file without problems.
    - Get DB URI from .env file and default to sqlite in memory if it's empty with printed message to notify the administrator.
    - suppress FSADeprecationWarning warning.

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ app.config.from_object('config')
 db = SQLAlchemy(app)
 
 
-# TODO: connect to a local postgresql database
+# DONE connect to a local postgresql database
 
 # ----------------------------------------------------------------------------#
 # Models.
--- config.py
+++ config.py
@@ -1,14 +1,17 @@
 import os
 
-SECRET_KEY = os.urandom(32)
+SECRET_KEY = os.getenv('SECRET_KEY')
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Enable debug mode.
-DEBUG = True
+# DEBUG = True #Enabling it in .env
 
 # Connect to the database
+# DONE IMPLEMENT DATABASE URL
+if not os.getenv('DATABASE_URI'):
+    print(' $ IMPORTANT INFO: using SQLITE in memory because there is no value for DATABASE_URI.')
+SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI') or 'sqlite:///:memory:'  # defaults to sqlite in memory
 
-
-# TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+# To suppress FSADeprecationWarning warning
+SQLALCHEMY_TRACK_MODIFICATIONS = False
